4777353ccfd4af45555aad49ddb485b370f4ff7c
Chris Hitchcott <hitchcott@gmail.com>
2018-11-24 11:23:53 +0700

    truffle boilerplate + RSK connection

---
+++ truffle.js
@@ -0,0 +1,17 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const ethDerivation = "m/44'/60'/0'/0/";
+const rskDerivation = "m/44'/137'/0'/0/";
+const rskTestnetDerivation = "m/44'/37310'/0'/0/";
+
+module.exports = {
+  networks: {
+    rsktestnet: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC.trim(), process.env.RSK_NODE, 0, 1, false, rskTestnetDerivation)
+
+      },
+      network_id: '31'
+    }
+  }
+};
