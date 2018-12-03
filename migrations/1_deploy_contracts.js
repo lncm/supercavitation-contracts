6e708849428b35fa5f6e67256892a83a821323cb
Chris Hitchcott <hitchcott@gmail.com>
2018-12-04 00:20:32 +0700

    recompile, deploy, bump npm version

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,12 @@
-// var RegistryStub = artifacts.require("RegistryStub");
 var SwapOffering = artifacts.require("SwapOffering");
 
- module.exports = function(deployer) {
-  // deployer.deploy(RegistryStub)
-  deployer.deploy(SwapOffering, 'https://lndtest.lncm.io/api', '0x0f18cd0F5B7CcE9d6DCC246F80B0fCdd7a2AF150');
- };
+const url = 'https://bob.lncm.io';
+const owner = '0x0f18cd0f5b7cce9d6dcc246f80b0fcdd7a2af150';
+// const satoshis = 100;
+
+ module.exports = async function(deployer) {;
+  await deployer.deploy(SwapOffering, url, owner);
+  // TODO fix this... ?
+  // console.log(`Funding contract with ${satoshis} satoshis of RBTC...`);
+  // await (await SwapOffering.deployed()).fundContract({ value: satoshis * 1e10, from: owner, gasPrice: 1, gas: 6000000 });
+};
