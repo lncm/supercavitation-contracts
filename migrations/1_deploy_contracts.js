319540c161f8a17fde9c7aefb7bd139434d4fbc6
Chris Hitchcott <hitchcott@gmail.com>
2018-12-15 17:04:38 +0100

    Fix mnemonic reading and migration, redeploy

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,11 +1,11 @@
 var SwapOffering = artifacts.require("SwapOffering");
 
-const url = 'https://bob.lncm.io';
-const owner = '0x0f18cd0f5b7cce9d6dcc246f80b0fcdd7a2af150';
-const satoshis = 1 * 1e8; // 1 btc
+const url = process.env.URL || 'http://localhost:8080';
+const satoshis = 1000;
 const wei = satoshis * 1e10;
 
- module.exports = (deployer) => {
+ module.exports = (deployer, network, [owner]) => {
+  console.log(`  Creating contract for ${url} with owner ${owner}...`)
   deployer.deploy(SwapOffering, url, owner)
   .then(() => SwapOffering.deployed())
   .then(c => {
--- truffle.js
+++ truffle.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
+const path = require('path');
 const HDWalletProvider = require("truffle-hdwallet-provider");
 
 const evmNode = process.env.RSK_NODE || 'https://public-node.testnet.rsk.co';
+
+const mnemonic = fs.readFileSync('./creds/mnemonic').toString().trim();
 
 module.exports = {
   networks: {
     rsktestnet: {
       provider: function() {
-        const mnemonic = fs.readFileSync('./creds/mnemonic').trim();
         if (!mnemonic) { throw "No mnemonic!"; }
         return new HDWalletProvider(mnemonic, evmNode, 0, 1, false, "m/44'/37310'/0'/0/");
       },
       gasPrice: 1,
+      gas: 6000000,
       network_id: '31'
     },
     ganache: {
